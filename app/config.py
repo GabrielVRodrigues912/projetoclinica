981a8788cd9b3a389dde4c674c537c9582cef4c7
GabrielVRodGabrielVRodrigues912 <gabriel.vrodrigues@aluno.faculdadeimpacta.com.br>
2024-09-09 23:37:55 -0300

    MYSQL CONECTION

--- app/config.py
+++ app/config.py
@@ -4,10 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class Config:
-    # SECRET_KEY = 'Sevencard@042'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://root:{SECRET_KEY}@localhost/clinica'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    TESTING = True
-
-
+    SECRET_KEY = 'Sevencard@042'
+    SQLALCHEMY_DATABASE_URI = 'mysql://admin:12345678@localhost/clinica'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    # TESTING = True
